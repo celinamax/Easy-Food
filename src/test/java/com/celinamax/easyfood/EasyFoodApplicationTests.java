16468c3cb895f11c3f3b1288425e2532857c01f6
Celina Maximiano <git config --global user.name Celina>
2019-04-20 19:22:11 -0300

    Projeto Criado

---
+++ src/test/java/com/celinamax/easyfood/EasyFoodApplicationTests.java
@@ -0,0 +1,16 @@
+package com.celinamax.easyfood;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasyFoodApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
