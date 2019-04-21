a0eef08df56ced85878937c77e92cc55ed4a5ee3
Celina Maximiano <git config --global user.name Celina>
2019-04-20 22:25:58 -0300

    Criando operação de instanciação

--- src/main/java/com/celinamax/easyfood/EasyFoodApplication.java
+++ src/main/java/com/celinamax/easyfood/EasyFoodApplication.java
@@ -1,13 +1,37 @@
 package com.celinamax.easyfood;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.celinamax.easyfood.domain.Categoria;
+import com.celinamax.easyfood.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class EasyFoodApplication {
+public class EasyFoodApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyFoodApplication.class, args);
+		
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
+		
+		
+		
 	}
 
 }
