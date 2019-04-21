58b6d54e5e20d8c5b7f0fd322b2ab79168d93d96
Celina Maximiano <git config --global user.name Celina>
2019-04-21 01:21:13 -0300

    Ajustes no endpoint / categorias/{id}

--- src/main/java/com/celinamax/easyfood/services/CategoriaService.java
+++ src/main/java/com/celinamax/easyfood/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.celinamax.easyfood.domain.Categoria;
 import com.celinamax.easyfood.repositories.CategoriaRepository;
+import com.celinamax.easyfood.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não Encontrado! Id: " + id 
+					+ ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 		
 	}
