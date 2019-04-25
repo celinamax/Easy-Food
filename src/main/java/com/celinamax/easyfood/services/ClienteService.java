3dff4722bbc62275b0ef0c5c8a8c177d517fa5ba
Celina Maximiano <git config --global user.name Celina>
2019-04-25 20:11:47 -0300

    Endpoint/clientes/{id} disponível

---
+++ src/main/java/com/celinamax/easyfood/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.celinamax.easyfood.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.celinamax.easyfood.domain.Cliente;
+import com.celinamax.easyfood.repositories.ClienteRepository;
+import com.celinamax.easyfood.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não Encontrado! Id: " + id 
+					+ ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+		
+	}
+
+}
