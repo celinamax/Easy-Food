fb2f763a5d8d6403b9372f0ae5e593d67425309c
Celina Maximiano <git config --global user.name Celina>
2019-04-27 22:10:50 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/celinamax/easyfood/services/CategoriaService.java
+++ src/main/java/com/celinamax/easyfood/services/CategoriaService.java
@@ -1,35 +1,47 @@
 package com.celinamax.easyfood.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.celinamax.easyfood.domain.Categoria;
 import com.celinamax.easyfood.repositories.CategoriaRepository;
+import com.celinamax.easyfood.services.exceptions.DataIntegrityException;
 import com.celinamax.easyfood.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Categoria obj = repo.findOne(id);
-		if(obj == null) {
-			throw new ObjectNotFoundException("Objeto não Encontrado! Id: " + id 
-					+ ", Tipo: " + Categoria.class.getName());
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não Encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
 		}
-		return obj;		
+		return obj;
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria com produtos!");
+
+		}
 	}
 
 }
