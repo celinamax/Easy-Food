ae7cc9f4223dd2478831f88d1c26bc6fd836ca18
Celina Maximiano <git config --global user.name Celina>
2019-04-28 22:36:15 -0300

    Validação customizada de CPF ou CNPJ na inserção de Cliente

--- src/main/java/com/celinamax/easyfood/dto/ClienteNewDTO.java
+++ src/main/java/com/celinamax/easyfood/dto/ClienteNewDTO.java
@@ -2,23 +2,47 @@ package com.celinamax.easyfood.dto;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.celinamax.easyfood.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message="Preenchimento Obrigatório!")
+	@Length(min=5, max=120, message="O tamanho deve ter entre 5 e 120 caracteres!" )
 	private String nome;
+	
+	@NotEmpty(message="Preenchimento Obrigatório!")
+	@Email(message="Email Inválido!")
 	private String email;
+	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
-	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private String logradouro;
+	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private String numero;
+	
 	private String complemento;
+	
 	private String bairro;
+	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private String cep;
 	
+	@NotEmpty(message="Preenchimento Obrigatório!")
 	private String telefone1;
+	
 	private String telefone2;
+	
 	private String telefone3;
 	
 	private Integer cidadeId;
---
+++ src/main/java/com/celinamax/easyfood/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,40 @@
+package com.celinamax.easyfood.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.celinamax.easyfood.domain.enums.TipoCliente;
+import com.celinamax.easyfood.dto.ClienteNewDTO;
+import com.celinamax.easyfood.resources.exception.FieldMessage;
+import com.celinamax.easyfood.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getcod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getcod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
