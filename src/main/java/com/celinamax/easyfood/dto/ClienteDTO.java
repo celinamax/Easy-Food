e78faef2f9f73900530f5036a7be3a2dbe6c8167
Celina Maximiano <git config --global user.name Celina>
2019-04-30 18:37:00 -0300

    Validação Customizada: email não repetido na atualização de Cliente

--- src/main/java/com/celinamax/easyfood/dto/ClienteDTO.java
+++ src/main/java/com/celinamax/easyfood/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.celinamax.easyfood.domain.Cliente;
+import com.celinamax.easyfood.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
