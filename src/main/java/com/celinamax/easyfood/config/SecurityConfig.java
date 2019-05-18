adfad69b8d4601b41cb47e1f273911feef8df691
Celina Maximiano <git config --global user.name Celina>
2019-05-18 18:21:48 -0300

    Implementando autorização

--- src/main/java/com/celinamax/easyfood/config/SecurityConfig.java
+++ src/main/java/com/celinamax/easyfood/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.celinamax.easyfood.security.JWTAuthenticationFilter;
+import com.celinamax.easyfood.security.JWTAuthorizationFilter;
 import com.celinamax.easyfood.security.JWTUtil;
 
 @Configuration
@@ -42,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			"/produtos/**",
 			"/categorias/**",	
 			"/clientes/**",
-			"/pedidos/**"
+			
 	};
 	
 	@Override
@@ -55,8 +56,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.authorizeRequests()
 		    .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
-			.anyRequest().authenticated();
+			.anyRequest().authenticated();		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);		
 	}
 	
