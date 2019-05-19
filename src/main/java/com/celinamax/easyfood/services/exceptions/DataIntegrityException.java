6c052a3021581d7b38076cdc3b4ea8322fe4c85c
Celina Maximiano <git config --global user.name Celina>
2019-05-18 22:49:33 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/celinamax/easyfood/services/exceptions/DataIntegrityException.java
+++ src/main/java/com/celinamax/easyfood/services/exceptions/DataIntegrityException.java
@@ -1,7 +1,6 @@
 package com.celinamax.easyfood.services.exceptions;
 
-public class DataIntegrityException extends RuntimeException{
-	
+public class DataIntegrityException extends RuntimeException{	
 	private static final long serialVersionUID = 1L;
 	
 	public DataIntegrityException(String msg) {
