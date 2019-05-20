b1bdd0ebfe3936444f92f380c65a6c24b4db13eb
Celina Maximiano <git config --global user.name Celina>
2019-05-19 21:05:08 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/celinamax/easyfood/EasyFoodApplication.java
+++ src/main/java/com/celinamax/easyfood/EasyFoodApplication.java
@@ -1,17 +1,24 @@
 package com.celinamax.easyfood;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.celinamax.easyfood.services.S3Service;
+
 @SpringBootApplication
 public class EasyFoodApplication implements CommandLineRunner{	
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyFoodApplication.class, args);		
 	}
 
 	@Override
-	public void run(String... args) throws Exception {		
+	public void run(String... args) throws Exception {	
+		s3Service.uploadFile("C:\\Users\\CELINA\\Desktop\\SISTEMAS DE INFORMAÇÃO\\1º SEMESTRE 2019\\Projeto\\Print EasyFood\\logo-v2.png");
 	}
 }
