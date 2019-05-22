40d83f804cffa2b97eb97dbdbe2651c19a797fed
Celina Maximiano <git config --global user.name Celina>
2019-05-22 11:55:50 -0300

    Liberando CORS para PUT e DELETE

--- src/main/java/com/celinamax/easyfood/config/SecurityConfig.java
+++ src/main/java/com/celinamax/easyfood/config/SecurityConfig.java
@@ -70,21 +70,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	}
 	
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
-		
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());		
 	}
 	
 	@Bean
 	  CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
 	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+	    source.registerCorsConfiguration("/**", configuration);
 	    return source;
 	  }
 	
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-		
+		return new BCryptPasswordEncoder();	
 	}
 
 }
